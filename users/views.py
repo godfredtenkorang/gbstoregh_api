9e84f9dc42b97f0e1175ab0d93051f7579db4959
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-12-26 15:17:55 +0000

    changes

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .serializers import RegisterSerializer, LoginSerializer
 from .models import User
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['POST'])
@@ -29,3 +30,12 @@ def login(request):
             "token": token.key
         }, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    try:
+        request.user.auth_token.delete()
+        return Response({"message": "Successfully logged out."}, status=status.HTTP_200_OK)
+    except (AttributeError, Token.DoesNotExist):
+        return Response({"error": "Invalid user."}, status=status.HTTP_400_BAD_REQUEST)
