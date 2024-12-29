e362e82bc46915c765a8e80ed0a6f0ce6a7df706
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-12-29 21:44:14 +0000

    changes

--- store/models.py
+++ store/models.py
@@ -46,17 +46,17 @@ class Product(models.Model):
         
     def get_image_1(self):
         if self.image1:
-            return "http://127.0.0.1:8000" + self.image1.url
+            return "https://api-gbstoregh.xyz" + self.image1.url
         return ''
     
     def get_image_2(self):
         if self.image1:
-            return "http://127.0.0.1:8000" + self.image2.url
+            return "https://api-gbstoregh.xyz" + self.image2.url
         return ''
     
     def get_image_3(self):
         if self.image1:
-            return "http://127.0.0.1:8000" + self.image3.url
+            return "https://api-gbstoregh.xyz" + self.image3.url
         return ''
         
     
